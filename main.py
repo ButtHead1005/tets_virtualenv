312a961cb79e85b3d1986ea2fcc1d41ef8144316
ButtHead1005 <kaliev1995ar@gmail.com>
2023-09-22 19:41:57 +0600

    tasks_env_two

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import numpy as np
-import matplotlib.pylab as plt
+import csv
 
-x = np.linspace(-np.pi, np.pi, 201)
-plt.plot(x, np.sin(x))
-plt.xlabel('Angle [rad]')
-plt.ylabel('sin(x)')
-plt.axis('tight')
-plt.show()
+with open('orders.csv','r') as csvinput:
+    with open('orders_two.csv', 'w') as csvoutput:
+        f_writer = csv.writer(csvoutput, lineterminator='\n')
+        f_reader = csv.reader(csvinput)
+        all_data = []
+        f_header = next(f_reader)
+        f_header.append('is_checked')
+        all_data.append(f_header)
+        
+        for row in f_reader:
+            row.append('checked')
+            all_data.append(row)
+        
+        f_writer.writerows(all_data)
